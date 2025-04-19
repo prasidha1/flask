aaa743963df66fdf109665f4cf2197c16b18060a
Prasidha R <32433336+prasidha1@users.noreply.github.com>
2025-04-19 05:34:50 +0530

    Update tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "<h1>Welcome to Flask tutorial</h1>"
+    
+@app.route("/<name>")
+def user(name):
+    return f"<h1>Welcome {name}</h1>"
+
+@app.route("/admin")
+def admin():
+    return (redirect(url_for("home")))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
